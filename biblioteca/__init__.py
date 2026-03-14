f94cb563dd2ebcebf7ec7ca405774a97633ecca5
giove2525 <giove2525@gmail.com>
2026-03-14 07:33:14 +0100

    Move credentials to .env

--- biblioteca/__init__.py
+++ biblioteca/__init__.py
@@ -2,15 +2,22 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 app = Flask(__name__, static_url_path='/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/bibliotecaDb'
-app.config['SECRET_KEY'] = '2227a0349dc08ade8b5f501a52c8c7d3'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
-from biblioteca import routes
-
-from flask_login import LoginManager
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
@@ -19,3 +26,5 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     from biblioteca.models import Utente
     return Utente.query.get(int(user_id))
+
+from biblioteca import routes
